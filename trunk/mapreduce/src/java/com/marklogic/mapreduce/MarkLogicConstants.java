3c027e727a401d9c6268d9990d27bd3ee9d351cd
kcoleman <kcoleman@62cac252-8da6-4816-9e9d-6dc37b19578c>
2011-03-25 19:47:23 +0000

    Javadoc changes only. Updated for document selector and sub-doc expr; cleaned up the internal use only constants and how constant values are represented in the javadoc.

--- trunk/mapreduce/src/java/com/marklogic/mapreduce/MarkLogicConstants.java
+++ trunk/mapreduce/src/java/com/marklogic/mapreduce/MarkLogicConstants.java
@@ -4,10 +4,10 @@
 package com.marklogic.mapreduce;
 
 /**
- * Constants used in the package, e.g. configuration property names.
- * Use these property names in your Hadoop configuration to set
- * MarkLogic specific properties. Properties may be set either in
- * a Hadoop configuration file, or programmatically.
+ * Configuration property names and other constants used in the
+ * package. Use these property names in your Hadoop configuration
+ * to set MarkLogic specific properties. Properties may be set
+ * either in a Hadoop configuration file or programatically.
  * 
  * <p>
  *  Use the <code>mapreduce.marklogic.input.*</code> properties when
@@ -22,7 +22,7 @@ package com.marklogic.mapreduce;
 public interface MarkLogicConstants {
 	// input-related config property names
 	/**
-	 * The config property name (<code>mapreduce.marklogic.com.input.username</code>)
+	 * The config property name (<code>{@value}</code>)
 	 * which, if set, specifies the MarkLogic Server user name
 	 * under which input queries and operations run. Required if using
 	 * MarkLogic Server for input.
@@ -30,7 +30,7 @@ public interface MarkLogicConstants {
     static final String INPUT_USERNAME = 
     	"mapreduce.marklogic.input.username";
     /**
-     * The config property name (<code>mapreduce.marklogic.input.password</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, specifies the cleartext password to use for 
      * authentication with {@link #INPUT_USERNAME input.username}. 
      * Required if using MarkLogic Server for input.
@@ -38,25 +38,39 @@ public interface MarkLogicConstants {
     static final String INPUT_PASSWORD = 
     	"mapreduce.marklogic.input.password";
     /**
-     * The config property name (<code>mapreduce.marklogic.input.host</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, specifies the MarkLogic Server host to use for
      * input operations. Required if using MarkLogic Server for input.
      */
     static final String INPUT_HOST = 
     	"mapreduce.marklogic.input.host";
     /**
-     * The config property name (<code>mapreduce.marklogic.input.port</code>)
-     * which, if set, specifies the port number of the input MarkLogic
-     * Server specified by the {@link #INPUT_HOST input.host} property.
-     * Required if using MarkLogic Server for input.
+     * The config property name (<code>{@value}</code>)
+     * which, if set, specifies the port number of the input XDBC 
+     * server on the MarkLogic Server host specified by the 
+     * {@link #INPUT_HOST input.host} property. Required if using 
+     * MarkLogic Server for input.
+     * 
+     * <p>
+     *  <strong>NOTE:</strong> Within a cluster, all nodes supplying 
+     *  MapReduce input data must use the same XDBC server port number.
+     * </p>
      */
     static final String INPUT_PORT = 
     	"mapreduce.marklogic.input.port";
     /**
-     * The config property name (<code>mapreduce.marklogic.input.documentSelector</code>)
-     * which, if set, specifies the path expression used to retrieve 
-     * documents from the server. Required if using MarkLogic Server for
-     * input in <code>basic</code> mode.
+     * The config property name (<code>{@value}</code>)
+     * which, if set, specifies the document selection portion of the
+     * path expression used to retrieve data from the server. Required
+     * if using MarkLogic Server for input in <code>basic</code> mode.
+     * 
+     * <p>
+     *  The XQuery path expression step given in this property must
+     *  select a sequence of document nodes. To further refine the
+     *  input selection to nodes or values within the documents, use 
+     *  {@link #SUBDOCUMENT_EXPRESSION input.subDocumentExpr}.
+     *  For more information, see the overview.
+     * </p>
      * 
      * <p>
      *  This property is only usable when <code>basic</code> mode is 
@@ -66,11 +80,11 @@ public interface MarkLogicConstants {
      *  with the {@link #INPUT_QUERY input.query} property.
      * </p>
      * 
-     * <p>The following would select all documents containing hrefs:</p>
+     * <p>The following selects all documents:</p>
      * 
      * <pre class="codesample">
      * &lt;property&gt;
-     *   &lt;name&gt;mapreduce.marklogic.input.pathexpression&lt;/name&gt;
+     *   &lt;name&gt;mapreduce.marklogic.input.documentSelector&lt;/name&gt;
      *   &lt;value&gt;fn:collection()&lt;/value&gt;
      * &lt;/property&gt;
      * </pre>
@@ -79,12 +93,20 @@ public interface MarkLogicConstants {
     	"mapreduce.marklogic.input.documentSelector";
     /**
      * The config property name 
-     * (<code>mapreduce.marklogic.input.subDocumentExpression</code>)
+     * (<code>{@value}</code>)
      * which, if set, specifies the path expression used to retrieve 
      * sub-document records from the server. Required if using MarkLogic 
-     * Server for input in <code>basic</code> mode and retrieving non-document 
-     * items.
+     * Server for input in <code>basic</code> mode and retrieving 
+     * non-document items.
      * 
+     * <p>
+     *  The XQuery path expression step given in this property should
+     *  select a sequence of nodes or atomic values from the set of
+     *  documents selected by the path step given in the
+     *  {@link #DOCUMENT_SELECTOR input.documentSelector} property. 
+     *  For more information, see the overview.
+     * </p>
+
      * <p>
      *  This property is only usable when <code>basic</code> mode is 
      *  specified with the {@link #INPUT_MODE input.mode} property. If
@@ -97,7 +119,11 @@ public interface MarkLogicConstants {
      * 
      * <pre class="codesample">
      * &lt;property&gt;
-     *   &lt;name&gt;mapreduce.marklogic.input.pathexpression&lt;/name&gt;
+     *   &lt;name&gt;mapreduce.marklogic.input.documentSelector&lt;/name&gt;
+     *   &lt;value&gt;fn:collection()&lt;/value&gt;
+     * &lt;/property&gt;
+     * &lt;property&gt;
+     *   &lt;name&gt;mapreduce.marklogic.input.subDocumentExpr&lt;/name&gt;
      *   &lt;value&gt;//wp:a[@href]&lt;/value&gt;
      * &lt;/property&gt;
      * </pre>
@@ -105,10 +131,11 @@ public interface MarkLogicConstants {
     static final String SUBDOCUMENT_EXPRESSION = 
     	"mapreduce.marklogic.input.subDocumentExpr";
     /**
-     * The config property name (<code>mapreduce.marklogic.input.namespace</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, specifies a list of namespaces to use when
-     * evaluating the path expression in the
-     * {@link #PATH_EXPRESSION input.pathexpression} property.
+     * evaluating the path expression constructed from the
+     * {@link #DOCUMENT_SELECTOR input.documentSelector} and
+     * {@link #SUBDOCUMENT_EXPRESSION input.subDocumentExpr} properties.
      * Required if using MarkLogic Server for input and running in
      * <code>basic</code> mode; see the 
      * {@link #INPUT_MODE input.mode} property.
@@ -132,9 +159,9 @@ public interface MarkLogicConstants {
     static final String PATH_NAMESPACE =
     	"mapreduce.marklogic.input.namespace";
     /**
-     * <em>Not yet implemented.</em>
+     * Not yet implemented.
      * <p>
-     *  The config property name (<code>mapreduce.marklogic.input.splitquery</code>)
+     *  The config property name (<code>{@value}</code>)
      *  which, if set, specifies the query MarkLogic Server uses 
      *  to generate input splits. Optional.
      * <p>
@@ -153,15 +180,15 @@ public interface MarkLogicConstants {
     static final String SPLIT_QUERY = 
     	"mapreduce.marklogic.input.splitquery";
     /**
-     *  The config property name (<code>mapreduce.marklogic.input.maxSplitSize</code>)
+     *  The config property name (<code>{@value}</code>)
      *  which, if set, specifies the maximum number of records per 
-     *  input split. Optional. Default: 1000. The default should be
-     *  suitable for most applications.
+     *  input split. Optional. Default: {@value #DEFAULT_MAX_SPLIT_SIZE}. 
+     *  The default should be suitable for most applications.
      */  
     static final String MAX_SPLIT_SIZE =
     	"mapreduce.marklogic.input.maxSplitSize";
     /**
-     * The config property name (<code>mapreduce.marklogic.input.databasename</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, specifies the name of the MarkLogic Server 
      * database from which to create input splits. Required if using
      * MarkLogic Server for input.
@@ -169,28 +196,30 @@ public interface MarkLogicConstants {
     static final String INPUT_DATABASE_NAME = 
     	"mapreduce.marklogic.input.databasename";
     /**
-     * The config property name (<code>mapreduce.marklogic.input.keyClass</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, specifies the name of the class of the map 
      * input keys. Optional. Default: {@link org.apache.hadoop.io.Text}.
      */
     static final String INPUT_KEY_CLASS = 
     	"mapreduce.marklogic.input.keyClass";
     /**
-     * The config property name (<code>mapreduce.marklogic.input.valueClass</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, specifies the name of the class of the map 
      * input value. Optional. Default: {@link org.apache.hadoop.io.Text}.
      */
     static final String INPUT_VALUE_CLASS = 
     	"mapreduce.marklogic.input.valueClass";
     /**
-     * The config property name (<code>mapreduce.marklogic.input.mode</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, specifies the whether to use basic or advanced
      * input query mode. Allowable values are <code>basic</code> and
      * <code>advanced</code>. Optional. Default: <code>basic</code>.
      * 
      * <p>
-     *  Basic mode enables use of the {@link #PATH_EXPRESSION input.pathexpression}
-     *  and {@link #PATH_NAMESPACE input.namespace} properties. Advanced
+     *  Basic mode enables use of the 
+     *  {@link #DOCUMENT_SELECTOR input.documentSelector},
+     *  {@link #SUBDOCUMENT_EXPRESSION input.subDocumentExpr}, and
+     *  {@link #PATH_NAMESPACE input.namespace} properties. Advanced
      *  mode enables use of the {@link #INPUT_QUERY input.query} and
      *  {@link #SPLIT_QUERY input.splitquery} properties.
      * </p>
@@ -206,7 +235,7 @@ public interface MarkLogicConstants {
      */
     static final String ADVANCED_MODE = "advanced";
     /**
-     * The config property name (<code>mapreduce.marklogic.input.query</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, specifies the query used to retrieve input
      * records from MarkLogic Server. This property is only usable
      * when <code>advanced</code> is specified in the
@@ -225,7 +254,7 @@ public interface MarkLogicConstants {
     
     // output-related config property names
 	/**
-	 * The config property name (<code>mapreduce.marklogic.com.output.username</code>)
+	 * The config property name (<code>{@value}</code>)
 	 * which, if set, specifies the MarkLogic Server user name
 	 * under which output operations run. Required if using MarkLogic
 	 * Server for output.
@@ -233,7 +262,7 @@ public interface MarkLogicConstants {
     static final String OUTPUT_USERNAME = 
     	"mapreduce.marklogic.output.username";
     /**
-     * The config property name (<code>mapreduce.marklogic.output.password</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, specifies the cleartext password to use for 
      * authentication with {@link #OUTPUT_USERNAME output.username}. 
      * Required if using MarkLogic Server for output.
@@ -241,7 +270,7 @@ public interface MarkLogicConstants {
     static final String OUTPUT_PASSWORD = 
     	"mapreduce.marklogic.output.password";
     /**
-     * The config property name (<code>mapreduce.marklogic.output.host</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, specifies the MarkLogic Server host to use for
      * output operations. Required if using MarkLogic Server for
      * output.
@@ -249,7 +278,7 @@ public interface MarkLogicConstants {
     static final String OUTPUT_HOST = 
     	"mapreduce.marklogic.output.host";
     /**
-     * The config property name (<code>mapreduce.marklogic.output.port</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, specifies the port number of the output MarkLogic
      * Server specified by the {@link #INPUT_HOST input.host} property.
      * Required if using MarkLogic Server for output.
@@ -257,7 +286,7 @@ public interface MarkLogicConstants {
     static final String OUTPUT_PORT = 
     	"mapreduce.marklogic.output.port";
     /**
-     * The config property name (<code>mapreduce.marklogic.output.directory</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, specifies the MarkLogic Server database directory
      * where output documents are created. Required if using MarkLogic
      * Server for output with document output format.
@@ -265,7 +294,7 @@ public interface MarkLogicConstants {
     static final String OUTPUT_DIRECTORY = 
     	"mapreduce.marklogic.output.directory";
     /**
-     * The config property name (<code>mapreduce.marklogic.output.collection</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, specifies a comma-separated list of collections
      * to which generated output documents are added. Optional. Relevant
      * only when using MarkLogic Server for output with document output
@@ -283,7 +312,7 @@ public interface MarkLogicConstants {
     static final String OUTPUT_COLLECTION =
     	"mapreduce.marklogic.output.collection";
     /**
-     * The config property name (<code>mapreduce.marklogic.output.permission</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, specifies a comma-separated list role-capability
      * pairs to associate with created output documents. Optional. If
      * not set, the default permissions for 
@@ -315,7 +344,7 @@ public interface MarkLogicConstants {
     static final String OUTPUT_PERMISSION = 
     	"mapreduce.marklogic.output.permission";
     /**
-     * The config property name (<code>mapreduce.marklogic.output.quality</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, specifies the document quality for created
      * output documents. Optional. Relevant only when using MarkLogic
      * Server for output with document output format.
@@ -331,7 +360,7 @@ public interface MarkLogicConstants {
     static final String OUTPUT_QUALITY = 
     	"mapreduce.marklogic.output.quality";
     /**
-     * The config property name (<code>mapreduce.marklogic.output.cleandir</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, indicates whether or not to remove the output
      * directory. Only applicable to document output. Default: true.
      * 
@@ -345,7 +374,7 @@ public interface MarkLogicConstants {
     static final String OUTPUT_CLEAN_DIR = 
     	"mapreduce.marklogic.output.cleandir";
     /**
-     * The config property name (<code>mapreduce.marklogic.output.nodeOpType</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, indicates what node operation to perform
      * during output. Required if using MarkLogic Server for output
      * with node output format.
@@ -357,25 +386,41 @@ public interface MarkLogicConstants {
     static final String NODE_OPERATION_TYPE = 
     	"mapreduce.marklogic.output.nodeOpType";
     /**
-     * The config property name (<code>mapreduce.marklogic.output.nodeOpType</code>)
+     * The config property name (<code>{@value}</code>)
      * which, if set, indicates the namespace used for output.
      */
     static final String OUTPUT_NAMESPACE = 
     	"mapreduce.marklogic.output.namespace";
     
     // internal constants
+    /** Internal Use Only */
     static final String USER_TEMPLATE = "{user}";
+    /** Internal Use Only */
     static final String PASSWORD_TEMPLATE = "{password}";
+    /** Internal Use Only */
     static final String HOST_TEMPLATE = "{host}";
+    /** Internal Use Only */
     static final String PORT_TEMPLATE = "{port}";
+    /** 
+     * The default maximum split size for input splits, used if
+     * {@link #MAX_SPLIT_SIZE input.maxSplitSize} is not specified.
+     */
     static final long DEFAULT_MAX_SPLIT_SIZE = 1000;
+    /** Internal Use Only */
 	static final String SERVER_URI_TEMPLATE =
     	"xcc://{user}:{password}@{host}:{port}";
+    /** Internal Use Only */
 	static final String DOCUMENT_SELECTOR_TEMPLATE = "{document_selector}";
+    /** Internal Use Only */
 	static final String SUBDOCUMENT_EXPR_TEMPLATE = "{subdocument-expr}";
+    /** Internal Use Only */
 	static final String NAMESPACE_TEMPLATE = "{namespace}";
+    /** Internal Use Only */
 	static final String DATABASENAME_TEMPLATE = "{database_name}";
+    /** Internal Use Only */
 	static final String QUERY_TEMPLATE = "{query}";
+    /** Internal Use Only */
 	static final String NODE_PATH_TEMPLATE = "{node_path}";
+    /** Internal Use Only */
     static final String NODE_STRING_TEMPLATE = "{node_string}";
 }
