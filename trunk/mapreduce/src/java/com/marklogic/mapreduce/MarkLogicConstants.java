b3333e5dbbb27e85770bfc54961d12baaa480166
kcoleman <kcoleman@62cac252-8da6-4816-9e9d-6dc37b19578c>
2011-06-10 23:03:10 +0000

    Javadoc only. Fixed some intra-page links.

--- trunk/mapreduce/src/java/com/marklogic/mapreduce/MarkLogicConstants.java
+++ trunk/mapreduce/src/java/com/marklogic/mapreduce/MarkLogicConstants.java
@@ -68,7 +68,7 @@ public interface MarkLogicConstants {
      * The config property name (<code>{@value}</code>)
      * which, if set, specifies the name of the class implementing 
      * SslConfigOptions which will be used if 
-     * (<code>@INPUT_USE_SSL</code>) is set to true.
+     * {@link #INPUT_USE_SSL input.ssl} is set to true.
      */
     static final String INPUT_SSL_OPTIONS_CLASS = 
         "mapreduce.marklogic.input.ssloptionsclass"; 
@@ -346,7 +346,7 @@ public interface MarkLogicConstants {
      * The config property name (<code>{@value}</code>)
      * which, if set, specifies the name of the class implementing 
      * SslConfigOptions which will be used if 
-     * (<code>@OUTPUT_USE_SSL</code>) is set to true.
+     * {@link #OUTPUT_USE_SSL output.usessl} is set to true.
      */
     static final String OUTPUT_SSL_OPTIONS_CLASS = 
         "mapreduce.marklogic.input.ssloptionsclass"; 
