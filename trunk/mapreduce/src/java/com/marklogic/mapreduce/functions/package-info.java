d1ab4d4762bed3b96450dc1f43ba6f27049f2934
kcoleman <kcoleman@62cac252-8da6-4816-9e9d-6dc37b19578c>
2011-09-22 23:43:55 +0000

    [Bug 15208] Javadoc only. Exclude some private classes from the javadoc. Also added brief javadoc descriptions for each of the packages.

---
+++ trunk/mapreduce/src/java/com/marklogic/mapreduce/functions/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Interfaces for using a MarkLogic Server lexicon as an input source.
+ * For details, see "Using MarkLogic Server for Input" in the
+ * <em>MarkLogic Connector for Hadoop Developer's Guide</em>.
+ */
+package com.marklogic.mapreduce.functions;
