3003941b3a449f8d7f1f5a43aed0db07bf0395a5
Aries Li <ali@marklogic.com>
2012-05-15 23:38:25 +0000

    Bug:17167 get the directory where the script itself stays

--- trunk/mlcp/src/main/mlcp.sh
+++ trunk/mlcp/src/main/mlcp.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-#mlcp.sh should be run under bin
 
-VMARGS="-DCONTENTPUMP_HOME=../lib -DCONTENTPUMP_VERSION=1.0"
-TARGET=../lib/marklogic-contentpump-1.0.jar
-LIB_HOME=../lib
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../
+#echo $DIR
+
+VMARGS="-DCONTENTPUMP_HOME=$DIR/lib -DCONTENTPUMP_VERSION=1.0"
+TARGET=$DIR/lib/marklogic-contentpump-1.0.jar
+LIB_HOME=$DIR/lib
 CLASSPATH=$LIB_HOME/commons-cli-1.2.jar
 CLASSPATH=$CLASSPATH:$LIB_HOME/commons-logging-1.1.1.jar
 CLASSPATH=$CLASSPATH:$LIB_HOME/marklogic-mapreduce-1.1.jar
@@ -11,6 +13,6 @@ CLASSPATH=$CLASSPATH:$LIB_HOME/marklogic-xcc-5.1.jar
 CLASSPATH=$CLASSPATH:$LIB_HOME/hadoop-core-0.20.2.jar
 CLASSPATH=$CLASSPATH:$LIB_HOME/marklogic-contentpump-1.0.jar
 CLASSPATH=$CLASSPATH:$LIB_HOME/commons-codec-1.3.jar
-echo $CLASSPATH
+#echo $CLASSPATH
 
 java -cp $CLASSPATH $VMARGS com.marklogic.contentpump.ContentPump $*
