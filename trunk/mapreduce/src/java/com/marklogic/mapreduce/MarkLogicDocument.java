93b93404d3bb4608c0824817a5bbf9251a8a42d1
Jane Chen <jchen@marklogic.com>
2012-05-31 23:10:06 +0000

    Bug:17389 - Add support for mixed document type of documents from MarkLogic.

---
+++ trunk/mapreduce/src/java/com/marklogic/mapreduce/MarkLogicDocument.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2003-2012 MarkLogic Corporation. All rights reserved.
+ */
+package com.marklogic.mapreduce;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableUtils;
+
+import com.marklogic.xcc.ResultItem;
+import com.marklogic.xcc.types.ValueType;
+
+/**
+ * Captures any type of MarkLogic documents.
+ *  
+ * @author jchen
+ *
+ */
+public class MarkLogicDocument implements Writable {
+    private byte[] content;
+    private ContentType contentType;
+    
+    public ContentType getContentType() {
+        return contentType;
+    }
+    
+    public Text getContentAsText() {
+        if (contentType == ContentType.XML || 
+            contentType == ContentType.TEXT) {
+            return new Text(content);         
+        }
+        throw new UnsupportedOperationException(
+        "Cannot convert binary data to Text.");
+    }
+    
+    public byte[] getContentAsByteArray() {
+        return content;
+    }
+    
+    public MarkLogicNode getContentAsMarkLogicNode() {
+        if (contentType == ContentType.XML || 
+            contentType == ContentType.TEXT) {
+            return new MarkLogicNode(getContentAsText().toString(), 
+                            contentType);      
+        }
+        throw new UnsupportedOperationException(
+                            "Cannot convert binary data to Text.");        
+    }
+    
+    public void set(ResultItem item) {
+        content = item.asString().getBytes();
+        if (item.getValueType() == ValueType.DOCUMENT) {
+            contentType = ContentType.XML;
+        } else if (item.getValueType() == ValueType.TEXT) {
+            contentType = ContentType.TEXT;
+        } else if (item.getValueType() == ValueType.BINARY) {
+            contentType = ContentType.BINARY;
+        } else {
+            contentType = ContentType.UNKNOWN;
+        }
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        int ordinal = in.readInt();
+        contentType = ContentType.valueOf(ordinal);
+        int length = WritableUtils.readVInt(in);
+        content = new byte[length];
+        in.readFully(content, 0, length);
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(contentType.ordinal());
+        WritableUtils.writeVInt(out, content.length);
+        out.write(content, 0, content.length);
+    }
+}
