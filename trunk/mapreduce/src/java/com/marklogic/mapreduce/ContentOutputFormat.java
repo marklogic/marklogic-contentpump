643668d9383d3ea19ae4894c953ee8172e2832ce
Jane Chen <jchen@marklogic.com>
2012-10-03 20:51:21 +0000

    Bug:19439 - Add Apache license header to source files.

--- trunk/mapreduce/src/java/com/marklogic/mapreduce/ContentOutputFormat.java
+++ trunk/mapreduce/src/java/com/marklogic/mapreduce/ContentOutputFormat.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2003-2012 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.marklogic.mapreduce;
 
 import java.io.IOException;
@@ -136,9 +151,9 @@ public class ContentOutputFormat<VALUEOUT> extends
                     }
                     String perm = perms[i].trim();
                     if (!perm.equalsIgnoreCase(ContentCapability.READ.toString()) &&
-                            !perm.equalsIgnoreCase(ContentCapability.EXECUTE.toString()) &&
-                            !perm.equalsIgnoreCase(ContentCapability.INSERT.toString()) &&
-                            !perm.equalsIgnoreCase(ContentCapability.UPDATE.toString())) {
+                        !perm.equalsIgnoreCase(ContentCapability.EXECUTE.toString()) &&
+                        !perm.equalsIgnoreCase(ContentCapability.INSERT.toString()) &&
+                        !perm.equalsIgnoreCase(ContentCapability.UPDATE.toString())) {
                         throw new IllegalStateException("Illegal capability: " + perm);
                     }
                     i++;
