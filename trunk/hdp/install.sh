aead7b80b76d70dd20e1c5837e7c9293c69ae884
Aries Li <ali@marklogic.com>
2012-10-16 07:18:26 +0000

    bug:19764 improve prompt

--- trunk/hdp/install.sh
+++ trunk/hdp/install.sh
@@ -10,9 +10,9 @@ checkArgs "$1" "$2" "$usage"
 while true; do
     read -p "Have you modified gsInstaller.properties? [y/n]" yn
     case $yn in
-        [Yy] ) echo "install..."; 
+        [Yy] | [yY][Ee][Ss] ) echo "install..."; 
         	cd HDP-gsInstaller-1.1.0.15/gsInstaller; sh gsPreRequisites.sh; sh createUsers.sh; sh gsInstaller.sh; break;;
-        [Nn] ) echo "Please modify gsInstaller.properties before running install.sh"; exit;;
+        [Nn] | [Nn][Oo] ) echo "Please modify gsInstaller.properties before running install.sh"; exit;;
         * ) echo "Please answer y or n.";;
     esac
 done
