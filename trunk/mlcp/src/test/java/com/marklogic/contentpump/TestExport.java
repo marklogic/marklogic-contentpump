931ebef772c970b4f2b3c19092a36c3065dbca46
Aries Li <ali@marklogic.com>
2013-03-21 05:07:55 +0000

    bug:21230 test case

--- trunk/mlcp/src/test/java/com/marklogic/contentpump/TestExport.java
+++ trunk/mlcp/src/test/java/com/marklogic/contentpump/TestExport.java
@@ -65,6 +65,57 @@ public class TestExport {
         assertEquals("5", result.next().asString());
         Utils.closeSession();
     }
+
+    @Test
+    public void testExportZip() throws Exception {
+        Utils.deleteDirectory(new File(Constants.OUT_PATH.toUri().getPath()));
+        String cmd = 
+            "IMPORT -host localhost -port 5275 -username admin -password admin"
+            + " -input_file_path " + Constants.TEST_PATH.toUri() + "/csv2.zip"
+            + " -delimited_uri_id first"
+            + " -input_compressed -input_compression_codec zip"
+            + " -input_file_type delimited_text";
+        
+        String[] args = cmd.split(" ");
+
+        Utils.clearDB("xcc://admin:admin@localhost:5275", "Documents");
+
+        String[] expandedArgs = null;
+        expandedArgs = OptionsFileUtil.expandArguments(args);
+        ContentPump.runCommand(expandedArgs);
+
+        ResultSequence result = Utils.runQuery(
+            "xcc://admin:admin@localhost:5275", "fn:count(fn:collection())");
+        assertTrue(result.hasNext());
+        assertEquals("5", result.next().asString());
+        Utils.closeSession();
+        
+        //export
+        cmd = "EXPORT -host localhost -port 5275 -username admin -password admin"
+            + " -output_file_path " + Constants.OUT_PATH.toUri()
+            + " -output_type document -compress";
+        args = cmd.split(" ");
+        expandedArgs = OptionsFileUtil.expandArguments(args);
+        ContentPump.runCommand(expandedArgs);
+        
+        //import it back
+        Utils.clearDB("xcc://admin:admin@localhost:5275", "Documents");
+
+        cmd = "import -host localhost -port 5275 -username admin -password admin"
+            + " -input_file_path " + Constants.OUT_PATH.toUri()
+            + " -input_file_type documents -document_type xml"
+            + " -input_compressed true";
+        args = cmd.split(" ");
+        expandedArgs = OptionsFileUtil.expandArguments(args);
+        ContentPump.runCommand(expandedArgs);
+        
+        result = Utils.runQuery(
+            "xcc://admin:admin@localhost:5275", "fn:count(fn:collection())");
+        assertTrue(result.hasNext());
+        assertEquals("5", result.next().asString());
+        Utils.closeSession();
+    }
+
     
     @Test
     public void testExportDocs() throws Exception {
