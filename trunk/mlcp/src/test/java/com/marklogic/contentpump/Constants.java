466143f69f53635d2375ffd797fd093abe27403c
Aries Li <ali@marklogic.com>
2013-04-08 08:34:07 +0000

    Bug:21094 update test

--- trunk/mlcp/src/test/java/com/marklogic/contentpump/Constants.java
+++ trunk/mlcp/src/test/java/com/marklogic/contentpump/Constants.java
@@ -1,5 +1,9 @@
 package com.marklogic.contentpump;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.apache.hadoop.fs.Path;
 
 /**
@@ -9,30 +13,50 @@ import org.apache.hadoop.fs.Path;
  *
  */
 public class Constants {
-    private static String WORKSPACE = "/space/workspace";
     /**
      * Home of mlcp. Must set if unit tests run in distributed mode
      */
-    public static String MLCP_HOME = WORKSPACE + "/xcc/mlcp";
-    
-    private static String TEST_DATA =  MLCP_HOME + "/src/test/resources";
+    public static String MLCP_HOME;
+    static{
+        try {
+            MLCP_HOME = new java.io.File( "." ).getCanonicalPath();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    private static String TEST_DATA = MLCP_HOME + "/src/test/resources";
     /**
      * Path of test data
      */
     public static Path TEST_PATH = new Path("file", null, TEST_DATA);
     
-    public static Path OUT_PATH = new Path("file", null, "/tmp/mlcpout");
+    public static Properties prop = new Properties(); 
+    static {
+        //load a properties file from class path, inside static method
+        try {
+            prop.load(new FileInputStream(MLCP_HOME + "/src/conf/test.properties"));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    public static Path OUT_PATH;
+    static {
+        String path = prop.getProperty("OUT_PATH", "/tmp/mlcpout");
+        OUT_PATH = new Path("file", null, path);
+    }
     
     /**
      * lib directory of mlcp binary package extracted. Must set if unit tests run in distributed mode
      */
-    public static String CONTENTPUMP_HOME = WORKSPACE + "/xcc/mlcp/target/marklogic-contentpump-1.1/lib";
+    public static String CONTENTPUMP_HOME = MLCP_HOME + "/target/marklogic-contentpump-1.1-lib";
     /**
      * mlcp version. Must set if unit tests run in distributed mode
      */
-    public static String CONTENTPUMP_VERSION = "1.1";
+    public static String CONTENTPUMP_VERSION = prop.getProperty("CONTENTPUMP_VERSION");
     /**
      * conf directory of hadoop. Must set if unit tests run in distributed mode
      */
-    public static String HADOOP_CONF_DIR = "/space/Downloads/hadoop-0.20.2/conf";
+    public static String HADOOP_CONF_DIR = prop.getProperty("HADOOP_CONF_DIR");
 }
