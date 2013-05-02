51028dbf1b94fa7b59d0a0c789396969080ebd2f
Aries Li <ali@marklogic.com>
2013-05-02 01:04:25 +0000

    Bug:21849 getHosts in non-fastload mode

--- trunk/mapreduce/src/java/com/marklogic/mapreduce/NodeOutputFormat.java
+++ trunk/mapreduce/src/java/com/marklogic/mapreduce/NodeOutputFormat.java
@@ -24,6 +24,7 @@ import org.apache.hadoop.mapreduce.RecordWriter;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 
 import com.marklogic.mapreduce.utilities.InternalUtilities;
+import com.marklogic.mapreduce.utilities.TextArrayWritable;
 import com.marklogic.xcc.ContentSource;
 
 /**
@@ -54,9 +55,9 @@ extends MarkLogicOutputFormat<NodePath, MarkLogicNode> {
     public RecordWriter<NodePath, MarkLogicNode> getRecordWriter(
             TaskAttemptContext context) throws IOException, InterruptedException {
         Configuration conf = context.getConfiguration();
-        LinkedMapWritable forestHostMap = getForestStatusMap(conf);
+        TextArrayWritable hosts = getHosts(conf);
         int taskId = context.getTaskAttemptID().getTaskID().getId();
-        String host = InternalUtilities.getHost(taskId, forestHostMap);
+        String host = InternalUtilities.getHost(taskId, hosts);
         return new NodeWriter(conf, host);
     }
 
