53ec7e65fa280f895d1c5fa0688e67162f6f3398
Aries Li <ali@marklogic.com>
2013-06-03 20:41:08 +0000

    test case

--- trunk/mlcp/src/test/java/com/marklogic/contentpump/TestDistributedImportDocs.java
+++ trunk/mlcp/src/test/java/com/marklogic/contentpump/TestDistributedImportDocs.java
@@ -44,9 +44,39 @@ public class TestDistributedImportDocs {
     }
     
     @Test
+    public void testImportTransformMixedDocs() throws Exception {
+        Utils.prepareModule("xcc://admin:admin@localhost:5275");
+        String cmd = "IMPORT -password admin -username admin -host localhost"
+            + " -input_file_path " + Constants.TEST_PATH.toUri() + "/wiki"
+            + " -port 5275 -output_uri_replace wiki,'wiki1'"
+            + " -output_uri_prefix test/"
+            + " -output_collections test,ML"
+            + " -fastload true"
+            + " -transform_module /lc.xqy"
+            + " -transform_namespace http://marklogic.com/module_invoke"
+            + " -hadoop_conf_dir " + Constants.HADOOP_CONF_DIR;
+        String[] args = cmd.split(" ");
+        assertFalse(args.length == 0);
+
+        Utils.clearDB("xcc://admin:admin@localhost:5275", "Documents");
+
+        String[] expandedArgs = null;
+        expandedArgs = OptionsFileUtil.expandArguments(args);
+        Utils.prepareDistributedMode();
+        ContentPump.runCommand(expandedArgs);
+
+        ResultSequence result = Utils.runQuery(
+            "xcc://admin:admin@localhost:5275",
+            "fn:count(fn:collection(\"ML\"))");
+        assertTrue(result.hasNext());
+        assertEquals("93", result.next().asString());
+        Utils.closeSession();
+    }
+    
+    @Test
     public void testImportText() throws Exception {
         String cmd = "IMPORT -password admin -username admin -host localhost"
-            + " -input_file_path " + Constants.TEST_PATH.toUri() + "/wiki/AbacuS"
+            + " -input_file_path " + Constants.TEST_PATH.toUri() + "/wiki/AbacuS.xml"
             + " -port 5275 -output_uri_prefix ABC"
             + " -output_collections test,ML -document_type text"
             + " -hadoop_conf_dir " + Constants.HADOOP_CONF_DIR;
