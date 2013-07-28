e1b6914e454d67971e240629bdca7a0069a274a0
Jane Chen <jchen@marklogic.com>
2013-07-28 21:40:56 +0000

    Revert change 145380 so that it works for dev.

--- trunk/mlcp/build.sh
+++ trunk/mlcp/build.sh
@@ -2,13 +2,12 @@
 
 # This script makes two mlcp binary bundles for each Hadoop version and put the
 # output in deliverable directory.
-DATE=`date +%Y%m%d`
 
 # install XCC jar
-mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-xcc -Dversion=7.0 -Dfile=../xcc/buildtmp/java/marklogic-xcc-7.0.${DATE}.jar -Dpackaging=jar
+mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-xcc -Dversion=7.0 -Dfile=../xcc/buildtmp/java/marklogic-xcc-7.0.jar -Dpackaging=jar
 # install connector jars
-mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-mapreduce1 -Dversion=1.2 -Dfile=../mapreduce/buildtmp/java/marklogic-mapreduce1-1.2.${DATE}.jar -Dpackaging=jar
-mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-mapreduce2 -Dversion=1.2 -Dfile=../mapreduce/buildtmp/java/marklogic-mapreduce2-1.2.${DATE}.jar -Dpackaging=jar
+mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-mapreduce1 -Dversion=1.2 -Dfile=../mapreduce/buildtmp/java/marklogic-mapreduce1-1.2.jar -Dpackaging=jar
+mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-mapreduce2 -Dversion=1.2 -Dfile=../mapreduce/buildtmp/java/marklogic-mapreduce2-1.2.jar -Dpackaging=jar
 # install hadoop-deps poms
 mvn install:install-file -DgroupId=com.marklogic -DartifactId=hadoop-deps -Dversion=1 -Dfile=hadoop-deps/pom-v1.xml -Dpackaging=pom
 mvn install:install-file -DgroupId=com.marklogic -DartifactId=hadoop-deps -Dversion=2 -Dfile=hadoop-deps/pom-v2.xml -Dpackaging=pom
