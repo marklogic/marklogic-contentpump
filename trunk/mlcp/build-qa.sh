402055932d854a0631904feacaadaae886dc199d
sverma <sverma@62cac252-8da6-4816-9e9d-6dc37b19578c>
2013-07-29 20:16:07 +0000

    adding build file for qa

---
+++ trunk/mlcp/build-qa.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# This script makes two mlcp binary bundles for each Hadoop version and put the
+# output in deliverable directory.
+DATE=`date +%Y%m%d`
+
+# install XCC jar
+mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-xcc -Dversion=7.0 -Dfile=../xcc/buildtmp/java/marklogic-xcc-7.0.${DATE}.jar -Dpackaging=jar
+# install connector jars
+mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-mapreduce1 -Dversion=1.2 -Dfile=../mapreduce/buildtmp/java/marklogic-mapreduce1-1.2.${DATE}.jar -Dpackaging=jar
+mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-mapreduce2 -Dversion=1.2 -Dfile=../mapreduce/buildtmp/java/marklogic-mapreduce2-1.2.${DATE}.jar -Dpackaging=jar
+# install hadoop-deps poms
+mvn install:install-file -DgroupId=com.marklogic -DartifactId=hadoop-deps -Dversion=1 -Dfile=hadoop-deps/pom-v1.xml -Dpackaging=pom
+mvn install:install-file -DgroupId=com.marklogic -DartifactId=hadoop-deps -Dversion=2 -Dfile=hadoop-deps/pom-v2.xml -Dpackaging=pom
+# prepare deliverable directory
+rm -rf deliverable
+mkdir deliverable
+# build mlcp-Hadoop1 
+mvn clean
+mvn package
+cp target/mlcp-Hadoop1* deliverable
+# build mlcp-Hadoop2  
+mvn clean
+mvn package -Dhadoop-deps.version=2
+cp target/mlcp-Hadoop2-*-bin.zip deliverable
