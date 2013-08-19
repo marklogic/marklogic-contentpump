02f19d481e92f022d7190af44a6a465b894b8be7
Jane Chen <jchen@marklogic.com>
2013-08-19 17:44:46 +0000

    Remove unused imports.

--- trunk/mlcp/src/main/java/com/marklogic/contentpump/Command.java
+++ trunk/mlcp/src/main/java/com/marklogic/contentpump/Command.java
@@ -27,11 +27,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import com.marklogic.mapreduce.ContentType;
 import com.marklogic.mapreduce.DocumentURI;
