520aa7e682110a1a4436ef18451043044335280e
Aries Li <ali@marklogic.com>
2013-09-07 01:04:57 +0000

    bug:23822

--- trunk/mlcp/src/main/java/com/marklogic/contentpump/CompressedDocumentReader.java
+++ trunk/mlcp/src/main/java/com/marklogic/contentpump/CompressedDocumentReader.java
@@ -172,11 +172,7 @@ public class CompressedDocumentReader<VALUEIN> extends
             baos.write(buf, 0, size);
         }
         if (value instanceof Text) {
-            if (encoding == null) {
-                ((Text) value).set(baos.toString());
-            } else {
-                ((Text) value).set(baos.toString(encoding));
-            }
+            ((Text) value).set(baos.toString(encoding));
         } else if (value instanceof BytesWritable) {
             if (batchSize > 1) {
                 // Copy data since XCC won't do it when Content is created.
