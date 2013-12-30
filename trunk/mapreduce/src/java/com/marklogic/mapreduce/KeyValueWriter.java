e8b2214bd897d26fdfbe4c1abfa09910c614e4d1
Jane Chen <jchen@marklogic.com>
2013-12-30 19:08:50 +0000

    Bug:25313 - Increment year on copyrights.

--- trunk/mapreduce/src/java/com/marklogic/mapreduce/KeyValueWriter.java
+++ trunk/mapreduce/src/java/com/marklogic/mapreduce/KeyValueWriter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2011 MarkLogic Corporation
+ * Copyright 2003-2014 MarkLogic Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 package com.marklogic.mapreduce;
 
 import java.io.IOException;
-import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
 
