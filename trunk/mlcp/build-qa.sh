e8bf8226c90156a849fd2bce0f2f7b9f601b5cdd
sverma <sverma@62cac252-8da6-4816-9e9d-6dc37b19578c>
2014-01-06 14:13:30 +0000

    updating the version

--- trunk/mlcp/build-qa.sh
+++ trunk/mlcp/build-qa.sh
@@ -7,8 +7,8 @@ DATE=`date +%Y%m%d`
 # install XCC jar
 mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-xcc -Dversion=7.0 -Dfile=../xcc/buildtmp/java/marklogic-xcc-7.0.${DATE}.jar -Dpackaging=jar
 # install connector jars
-mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-mapreduce1 -Dversion=1.2 -Dfile=../mapreduce/buildtmp/java/marklogic-mapreduce1-1.2.${DATE}.jar -Dpackaging=jar
-mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-mapreduce2 -Dversion=1.2 -Dfile=../mapreduce/buildtmp/java/marklogic-mapreduce2-1.2.${DATE}.jar -Dpackaging=jar
+mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-mapreduce1 -Dversion=1.2 -Dfile=../mapreduce/buildtmp/java/marklogic-mapreduce1-2.1.${DATE}.jar -Dpackaging=jar
+mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-mapreduce2 -Dversion=1.2 -Dfile=../mapreduce/buildtmp/java/marklogic-mapreduce2-2.1.${DATE}.jar -Dpackaging=jar
 # install hadoop-deps poms
 mvn install:install-file -DgroupId=com.marklogic -DartifactId=hadoop-deps -Dversion=1 -Dfile=hadoop-deps/pom-v1.xml -Dpackaging=pom
 mvn install:install-file -DgroupId=com.marklogic -DartifactId=hadoop-deps -Dversion=2 -Dfile=hadoop-deps/pom-v2.xml -Dpackaging=pom
