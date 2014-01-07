a9c93923285cec973dc5b023042cd41d4ad5b286
sverma <sverma@62cac252-8da6-4816-9e9d-6dc37b19578c>
2014-01-07 19:22:33 +0000

    fixing xcc version

--- trunk/mlcp/build-qa.sh
+++ trunk/mlcp/build-qa.sh
@@ -5,7 +5,7 @@
 DATE=`date +%Y%m%d`
 
 # install XCC jar
-mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-xcc -Dversion=7.0 -Dfile=../xcc/buildtmp/java/marklogic-xcc-7.0.${DATE}.jar -Dpackaging=jar
+mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-xcc -Dversion=8.0 -Dfile=../xcc/buildtmp/java/marklogic-xcc-8.0.${DATE}.jar -Dpackaging=jar
 # install connector jars
 mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-mapreduce1 -Dversion=1.2 -Dfile=../mapreduce/buildtmp/java/marklogic-mapreduce1-2.1.${DATE}.jar -Dpackaging=jar
 mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-mapreduce2 -Dversion=1.2 -Dfile=../mapreduce/buildtmp/java/marklogic-mapreduce2-2.1.${DATE}.jar -Dpackaging=jar
