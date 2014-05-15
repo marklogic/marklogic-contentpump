07fbadf5b6658bac85b7a6e6fa7ff0db9becd994
Aries Li <ali@marklogic.com>
2014-05-15 18:15:43 +0000

    bug:26968 remove hadoop2.0.4 support

--- trunk/mlcp/src/main/mlcp.sh
+++ trunk/mlcp/src/main/mlcp.sh
@@ -3,17 +3,9 @@ unset CLASSPATH
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../
 VMARGS="-DCONTENTPUMP_HOME=$DIR/lib" 
 LIB_HOME=$DIR/lib
-if [[ -z $HADOOP_MAPREDUCE_VERSION || $HADOOP_MAPREDUCE_VERSION == "1"  ]] ; then
-    EXCLUS=*hadoop-*-2.0.4-*
-else
-    EXCLUS=*hadoop-*-2.0.0-*
-fi
 
 for file in ${LIB_HOME}/*.jar
 do
-  if [[ $file == ${EXCLUS} ]] ; then
-    continue
-  fi
   if [ ! -z "$CLASSPATH" ]; then
     CLASSPATH=${CLASSPATH}":"$file
   else
