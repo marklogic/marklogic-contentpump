3c266d1aeefc49114306306a42b28ec270f7069e
Aries Li <ali@marklogic.com>
2014-11-11 00:33:24 +0000

    mlcp unit test for rdf

--- trunk/mlcp/src/test/java/com/marklogic/contentpump/TestRDF.java
+++ trunk/mlcp/src/test/java/com/marklogic/contentpump/TestRDF.java
@@ -188,6 +188,56 @@ public class TestRDF {
         
         Utils.closeSession();
     }    
+ 
+    @Test
+    public void testTurtlePermissionNonAdmin() throws Exception {
+        String cmd =
+                "IMPORT -host localhost -port 5275 -username admin -password admin"
+                        + " -input_file_path " + Constants.TEST_PATH.toUri() + "/semantics.ttl"
+                        + " -output_permissions rest-admin,read,rest-admin,update"
+                        + " -input_file_type rdf -rdf_streaming_memory_threshold " + threshold;
+
+        String[] args = cmd.split(" ");
+
+        Utils.clearDB("xcc://admin:admin@localhost:5275", "Documents");
+
+        String[] expandedArgs = null;
+        expandedArgs = OptionsFileUtil.expandArguments(args);
+        ContentPump.runCommand(expandedArgs);
+
+        ResultSequence result = Utils.runQuery(
+                "xcc://admin:admin@localhost:5275", "declare namespace sem=\"http://marklogic.com/semantics\"; fn:count(/sem:triples)");
+        assertTrue(result.hasNext());
+        assertEquals("8", result.next().asString());
+
+        result = Utils.runQuery(
+                "xcc://admin:admin@localhost:5275", "declare namespace sem=\"http://marklogic.com/semantics\"; fn:count(//sem:triple)");
+        assertTrue(result.hasNext());
+        assertEquals("795", result.next().asString());
+
+        result = Utils.runQuery(
+                "xcc://admin:admin@localhost:5275", "declare namespace sem=\"http://marklogic.com/semantics\"; fn:count(/sem:triples/sem:triple[sem:subject = \"http://www.w3.org/TR/rdf-syntax-grammar\"])");
+        assertTrue(result.hasNext());
+        assertEquals("2", result.next().asString());
+
+        result = Utils.runQuery(
+            "xcc://admin:admin@localhost:5275", "declare namespace sem=\"http://marklogic.com/semantics\"; fn:count(/sem:graph)");
+        assertTrue(result.hasNext());
+        assertEquals("1", result.next().asString());
+        
+        result = Utils.runQuery(
+            "xcc://admin:admin@localhost:5275", "declare namespace sem=\"http://marklogic.com/semantics\"; fn:count(fn:doc(\"http://marklogic.com/semantics#default-graph\"))");
+        assertTrue(result.hasNext());
+        assertEquals("1", result.next().asString());
+        
+        result = Utils.runQuery(
+            "xcc://admin:admin@localhost:5275", "fn:count(for $doc in fn:doc() return (xdmp:document-get-permissions(fn:base-uri($doc))))"
+        );
+        assertTrue(result.hasNext());
+        assertEquals("18", result.next().asString());
+        
+        Utils.closeSession();
+    }    
     
     @Test
     public void testRDFJSON() throws Exception {
