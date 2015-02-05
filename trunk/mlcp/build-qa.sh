cf1ccfe3c841ca022a4b75e66ed2b18d09d413c4
sverma <sverma@62cac252-8da6-4816-9e9d-6dc37b19578c>
2015-02-05 22:41:10 +0000

    Fixing versions for HEAD/9.0

--- trunk/mlcp/build-qa.sh
+++ trunk/mlcp/build-qa.sh
@@ -5,9 +5,9 @@
 DATE=`date +%Y%m%d`
 
 # install XCC jar
-mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-xcc -Dversion=8.0 -Dfile=../xcc/buildtmp/java/marklogic-xcc-8.0.${DATE}.jar -Dpackaging=jar
+mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-xcc -Dversion=9.0 -Dfile=../xcc/buildtmp/java/marklogic-xcc-9.0.${DATE}.jar -Dpackaging=jar
 # install connector jars
-mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-mapreduce2 -Dversion=2.1 -Dfile=../mapreduce/buildtmp/java/marklogic-mapreduce2-2.1.${DATE}.jar -Dpackaging=jar
+mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-mapreduce2 -Dversion=2.2 -Dfile=../mapreduce/buildtmp/java/marklogic-mapreduce2-2.2.${DATE}.jar -Dpackaging=jar
 
 # prepare deliverable directory
 rm -rf deliverable
