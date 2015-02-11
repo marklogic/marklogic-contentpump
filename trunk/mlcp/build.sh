f02013090b9460156e350fa527eb33f2780028ea
jhoy <jhoy@62cac252-8da6-4816-9e9d-6dc37b19578c>
2015-02-11 15:33:51 +0000

    [jh] updated versions of xcc to 9.0 and mapreduce to 2-2.2

--- trunk/mlcp/build.sh
+++ trunk/mlcp/build.sh
@@ -4,9 +4,9 @@
 # output in deliverable directory.
 
 # install XCC jar
-mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-xcc -Dversion=8.0 -Dfile=../xcc/buildtmp/java/marklogic-xcc-8.0.jar -Dpackaging=jar
+mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-xcc -Dversion=9.0 -Dfile=../xcc/buildtmp/java/marklogic-xcc-9.0.jar -Dpackaging=jar
 # install connector jars
-mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-mapreduce2 -Dversion=2.1 -Dfile=../mapreduce/buildtmp/java/marklogic-mapreduce2-2.1.jar -Dpackaging=jar
+mvn install:install-file -DgroupId=com.marklogic -DartifactId=marklogic-mapreduce2 -Dversion=2.2 -Dfile=../mapreduce/buildtmp/java/marklogic-mapreduce2-2.2.jar -Dpackaging=jar
 
 # prepare deliverable directory
 rm -rf deliverable
