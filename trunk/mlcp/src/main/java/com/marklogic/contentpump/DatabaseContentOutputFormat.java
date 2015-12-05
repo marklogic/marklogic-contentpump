147c53ce52253519b234fc1e5a87eeccb504d68c
Jane Chen <jchen@marklogic.com>
2015-12-05 05:48:27 +0000

    Bug:36189 - Revert.

--- trunk/mlcp/src/main/java/com/marklogic/contentpump/DatabaseContentOutputFormat.java
+++ trunk/mlcp/src/main/java/com/marklogic/contentpump/DatabaseContentOutputFormat.java
@@ -39,7 +39,8 @@ public class DatabaseContentOutputFormat extends
     public RecordWriter<DocumentURI, DatabaseDocumentWithMeta> getRecordWriter(
         TaskAttemptContext context) throws IOException, InterruptedException {
         Configuration conf = context.getConfiguration();
-        Map<String, ContentSource> sourceMap = getSourceMap(context);
+        fastLoad = Boolean.valueOf(conf.get(OUTPUT_FAST_LOAD));
+        Map<String, ContentSource> sourceMap = getSourceMap(fastLoad, context);
         // construct the DatabaseContentWriter
         return new DatabaseContentWriter<DatabaseDocumentWithMeta>(conf,
             sourceMap, fastLoad, am);
