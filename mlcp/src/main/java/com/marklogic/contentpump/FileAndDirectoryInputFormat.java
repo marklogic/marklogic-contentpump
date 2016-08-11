220d14a8b06ef136aced6fcee6ff0f25a9f30aae
Matt Sun <sjf.matt@gmail.com>
2016-08-10 17:43:25 -0700

    bug:41273 bug:41455 Don't call getFileBlockLocation for directories on HDFS

--- mlcp/src/main/java/com/marklogic/contentpump/FileAndDirectoryInputFormat.java
+++ mlcp/src/main/java/com/marklogic/contentpump/FileAndDirectoryInputFormat.java
@@ -27,6 +27,7 @@ import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.PathFilter;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -80,10 +81,13 @@ FileInputFormat<K, V> {
             for (FileStatus child : files) {
                 Path path = child.getPath();
                 FileSystem fs = path.getFileSystem(conf);
-                // length is 0 for dir according to FSDirectory.java in 0.20
+                // Only for directory on HDFS, the length is always zero
+                // If getFileBlockLocations is called with directory on HDFS,
+                // An exception will throw. See bug:24988
                 long length = child.getLen();
                 BlockLocation[] blkLocations = null;
-                if (!child.isDirectory()) {
+                if (!(child.isDirectory() && 
+                        fs instanceof DistributedFileSystem)) {
                     blkLocations = fs.getFileBlockLocations(child, 0, length);
                 } else if (length != 0) {
                     throw new IOException("non-zero length directory on HDFS:"
