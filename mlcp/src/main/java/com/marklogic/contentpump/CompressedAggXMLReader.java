2629051ec486f08ce54bffa8aa793ec822048386
Matt Sun <sjf.matt@gmail.com>
2017-06-09 11:03:15 -0700

    bug:43472

--- mlcp/src/main/java/com/marklogic/contentpump/CompressedAggXMLReader.java
+++ mlcp/src/main/java/com/marklogic/contentpump/CompressedAggXMLReader.java
@@ -130,7 +130,7 @@ public class CompressedAggXMLReader<VALUEIN> extends
             }
         } else {
             throw new UnsupportedOperationException("Unsupported codec: "
-                + codec.name());
+                + codecString);
         }    
         if (useAutomaticId) {
             idGen = new IdGenerator(file.toUri().getPath() + "-"
--- mlcp/src/main/java/com/marklogic/contentpump/CompressedDelimitedTextReader.java
+++ mlcp/src/main/java/com/marklogic/contentpump/CompressedDelimitedTextReader.java
@@ -87,6 +87,9 @@ public class CompressedDelimitedTextReader extends DelimitedTextReader<Text> {
             .toString())) {
             zipIn = new GZIPInputStream(fileIn);
             codec = CompressionCodec.GZIP;
+        } else {
+            throw new UnsupportedOperationException("Unsupported codec: "
+                    + codecString);
         }
         
         if (uriName == null) {
